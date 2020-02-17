a111e26cc6ee9ec661b92e0cce899eca7f16c5f8
Eduardo Santana <eduardoamparos@gmail.com>
2020-02-16 21:05:49 -0500

    Adding Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OhifViewerV2 from './ohif-viewer-v2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <OhifViewerV2 props={{
+                routerBasename: '/',
+                extensions: [],
+                showStudyList: false,
+                servers: {
+                  dicomWeb: [
+                    {
+                      name: 'DCM4CHEE',
+                      wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+                      qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+                      wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+                      studyInstanceUids: '1.2.826.0.13854362241694438965858641723883466450351448',
+                      qidoSupportsIncludeField: true,
+                      imageRendering: 'wadors',
+                      thumbnailRendering: 'wadors',
+                    },
+                  ],
+                },
+              }}/>  
     </div>
   );
 }
---
+++ src/ohif-viewer-v2.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { App } from '@ohif/viewer';
+// <App props={props} />
+const OhifViewerV2 = (props) => {
+  return (
+    <App props={props} />
+  );
+};
+
+export default OhifViewerV2;
